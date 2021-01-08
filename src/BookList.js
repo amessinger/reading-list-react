bf6c1dc9e6974179f4b1e8011c86ce1ff11ab393
Antonin Messinger <antonin.messinger@gmail.com>
2021-01-08 16:22:15 +0100

    style book list

--- src/BookList.js
+++ src/BookList.js
@@ -5,12 +5,22 @@ import {
   Link,
   useRouteMatch,
 } from 'react-router-dom';
+import {
+  IconButton,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+} from '@material-ui/core/';
+import InfoIcon from '@material-ui/icons/Info';
 import { StoreContext } from './store';
 import BookDetail from './BookDetail';
 
 export default function BookList() {
   const { books: [bookList] } = useContext(StoreContext);
   const currentRoute = useRouteMatch();
+  const iconButtonStyle = {
+    color: 'rgba(255, 255, 255, 0.54)',
+  };
 
   if (!bookList?.length) {
     <div data-test="BookList">
@@ -20,13 +30,26 @@ export default function BookList() {
 
   return (
     <div data-test="BookList">
-      {bookList.map(book => <Link to={`${currentRoute.url}/${book.id}`} key={book.id}>{book.title}</Link>)}
+      <GridList>
+        {bookList.map(book => (
+          <GridListTile key={book.id}>
+            <img src={book.cover} alt={`${book.title} cover`} />
+            <Link to={`${currentRoute.url}/${book.id}`}>{book.title}</Link>
+            <GridListTileBar
+              title={book.title}
+              subtitle={<span>by: {book.author}</span>}
+              actionIcon={
+                <IconButton aria-label={`More info about ${book.title}`} style={iconButtonStyle} component={Link} to={`${currentRoute.url}/${book.id}`}>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
       <Switch>
         <Route path={`${currentRoute.path}/:bookId`}>
           <BookDetail />
-        </Route>
-        <Route path={currentRoute.path}>
-          <p>Please select a book</p>
         </Route>
       </Switch>
     </div>
