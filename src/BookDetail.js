f95b43f34be61f2d0465aba50d76d2eb6138ad5c
Antonin Messinger <antonin.messinger@gmail.com>
2021-01-08 19:25:05 +0100

    move link from book detail to list to bottom of card

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -21,6 +21,13 @@ export default function BookDetail() {
     height: 0,
     paddingTop: '56.25%',
   };
+  const bottomCardStyle = {
+    marginTop: '1em',
+    marginBottom: '1em',
+    button: {
+      width: '100%'
+    }
+  };
 
   if (!book) {
     return (
@@ -30,33 +37,37 @@ export default function BookDetail() {
     );
   }
   return (
-    <Card data-test="BookDetail">
-      <CardHeader
-        title={book.title}
-        subheader={`by ${book.author}`}
-        action={<Button variant="outlined" component={Link} to='/books'>Book list</Button>}
-      />
-      <CardMedia
-        style={cardMediaStyle}
-        image={book.cover}
-        title={`${book.title} cover`}
-      />
-      <CardContent>{book.description ? book.description : 'No description.'}</CardContent>
-      <List dense={true}>
-        <ListItem>
-          <ListItemText primary="Publisher" secondary={book.publisher} />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="ISBN" secondary={book.isbn} />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="By the same author" />
-        </ListItem>
-        {booksBySameAuthor.length
-          ? booksBySameAuthor.map(book => <ListItem key={book.id} component={Link} to={`/books/${book.id}`}><ListItemText primary={book.title} /></ListItem>)
-          : <ListItem><ListItemText primary="None" /></ListItem>
-        }
-      </List>
-    </Card>
+    <>
+      <Card data-test="BookDetail">
+        <CardHeader
+          title={book.title}
+          subheader={`by ${book.author}`}
+        />
+        <CardMedia
+          style={cardMediaStyle}
+          image={book.cover}
+          title={`${book.title} cover`}
+        />
+        <CardContent>{book.description ? book.description : 'No description.'}</CardContent>
+        <List dense={true}>
+          <ListItem>
+            <ListItemText primary="Publisher" secondary={book.publisher} />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="ISBN" secondary={book.isbn} />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="By the same author" />
+          </ListItem>
+          {booksBySameAuthor.length
+            ? booksBySameAuthor.map(book => <ListItem key={book.id} component={Link} to={`/books/${book.id}`}><ListItemText primary={book.title} /></ListItem>)
+            : <ListItem><ListItemText primary="None" /></ListItem>
+          }
+        </List>
+      </Card>
+      <Card style={bottomCardStyle}>
+        <Button style={bottomCardStyle.button} component={Link} to="/books">Back to book list</Button>
+      </Card>
+    </>
   );
 }
